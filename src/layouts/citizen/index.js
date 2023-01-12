e8d0db811648d7213b7f4cc8eac18fa74da6c0bd
dilane3 <komboudilane125@gmail.com>
2023-01-12 02:48:26 +0100

    ADD: Renaming some folders into admin and citizen

---
+++ src/layouts/citizen/index.js
@@ -0,0 +1,94 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+// Data
+import authorsTableData from "layouts/citizen/data/authorsTableData";
+import MDButton from "components/MDButton";
+
+import Box from "@mui/material/Box";
+
+function Citizens() {
+  const { columns, rows } = authorsTableData();
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+                style={{
+                  flexDirection: "row",
+                }}
+              >
+                <MDTypography variant="h6" color="white">
+                  Liste des citoyens
+                </MDTypography>
+
+                <Box
+                  sx={{
+                    display: "flex",
+                    position: "absolute",
+                    right: 30,
+                    top: -5,
+                  }}
+                >
+                  <MDButton color="white" size="sm" variant="contained">
+                    Enregistrer un citoyen
+                  </MDButton>
+                </Box>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Citizens;
