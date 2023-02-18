1b6a49ad93f2c0fdc04dc7ab3188b54ddde1a94b
dilane3 <komboudilane125@gmail.com>
2023-02-18 13:44:59 +0100

    Update Card position

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,27 +3,40 @@ export const generateImage = () => {
 
   const random = Math.floor(Math.random() * 50);
 
-  return `https://i.pravatar.cc/150?img=${random}`; 
-}
+  return `https://i.pravatar.cc/150?img=${random}`;
+};
 
 export const formatDate = (d) => {
   const date = new Date(d);
 
   console.log({
     d,
-    date
-  })
+    date,
+  });
 
   const day = date.getDate();
   const month = date.getMonth();
   const year = date.getFullYear();
 
-  const months = ["Jan", "Fev", "Mar", "Avr", "Mai", "Juin", "Juil", "Aout", "Sept", "Oct", "Nov", "Dec"]
+  const months = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Avr",
+    "Mai",
+    "Juin",
+    "Juil",
+    "Aout",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
   return `${day <= 9 ? `0${day}` : day} ${months[month]} ${year}`;
-}
+};
 
 export const getUrlOfServer = () => {
-  return "https://cnic.onrender.com/"
+  return "https://cnic.onrender.com/";
   // return "http://localhost:5000/"
-}
+};
